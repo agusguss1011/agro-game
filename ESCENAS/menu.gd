edf56d7e82567292a217462eaa0c6656f1607880
agusguss1011 <agusguss.1011@gmail.com>
2025-10-04 21:57:21 -0300

    2da Actualizacion

--- ESCENAS/menu.gd
+++ ESCENAS/menu.gd
@@ -1,5 +1,10 @@
 extends Control
-
+func _ready():
+	$VBoxContainer/STARTButton.grab_focus()
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://node_2d.tscn")
+
+
+func _on_quit_button_2_pressed() -> void:
+	get_tree().quit()
---
+++ test.gd
@@ -0,0 +1,4 @@
+extends Area2D
+func on_Area2D_body_entered(body):
+	queue_free()
+pass
